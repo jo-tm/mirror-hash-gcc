e7d3d1e5bd487ea2ad76aea61f43dd9916194324
José <30682875+jo-tm@users.noreply.github.com>
2023-04-11 22:45:05 +0200

    Update mirror256.c

--- mirror256.c
+++ mirror256.c
@@ -1,37 +1,62 @@
-#include <stdio.h>
 #include "mirror256.h"
+#include <string.h>
+#include <math.h>
 
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        printf("Usage: %s <input_file>\n", argv[0]);
-        return 1;
+#include "mirror256.h"
+#include <string.h>
+#include <math.h>
+
+// Helper functions
+void init_standard_state(uint8_t state[][MIRROR256_DEFAULT_SIZE / 4]) {
+    // Implement the initStandardState logic from the Python code here.
+}
+
+void init_last_hashes(uint8_t last_hashes[][MIRROR256_DEFAULT_SIZE / 4]) {
+    // Implement the initLastHashes logic from the Python code here.
+}
+
+void mirror256_init(mirror256_context *ctx) {
+    memset(ctx->buffer, 0, sizeof(ctx->buffer));
+    ctx->counter = 0;
+    ctx->depth = MIRROR256_DEFAULT_DEPTH;
+    ctx->size = MIRROR256_DEFAULT_SIZE;
+
+    // Initialize standard state
+    uint8_t standard_state[MIRROR256_DEFAULT_DEPTH][MIRROR256_DEFAULT_SIZE / 4];
+    init_standard_state(standard_state);
+
+    // Initialize last_hashes with initLastHashes logic
+    init_last_hashes(ctx->last_hashes);
+
+    // Copy standard state to last_hashes
+    for (int i = 0; i < ctx->depth; i++) {
+        memcpy(ctx->last_hashes[i], standard_state[i], MIRROR256_DEFAULT_SIZE / 4);
+    }
+}
+
+void mirror256_update(mirror256_context *ctx, const uint8_t *data, size_t len) {
+    size_t buffer_len = ctx->counter % 32;
+    size_t available = 32 - buffer_len;
+
+    while (len > 0) {
+        size_t to_copy = len < available ? len : available;
+        memcpy(ctx->buffer + buffer_len, data, to_copy);
+        data += to_copy;
+        len -= to_copy;
+        buffer_len += to_copy;
+
+        if (buffer_len == 32) {
+            // Process the buffer using _mirror256_process() logic from the Python code.
+            buffer_len = 0;
+        }
+
+        available = 32 - buffer_len;
     }
 
-    FILE *file = fopen(argv[1], "rb");
-    if (file == NULL) {
-        perror("Error opening file");
-        return 1;
-    }
+    ctx->counter += len;
+}
 
-    uint8_t buffer[1024];
-    size_t len;
-    mirror256_context ctx;
-    mirror256_init(&ctx);
-
-    while ((len = fread(buffer, 1, sizeof(buffer), file)) > 0) {
-        mirror256_update(&ctx, buffer, len);
-    }
-
-    fclose(file);
-
-    uint8_t digest[32];
-    mirror256_final(&ctx, digest);
-
-    printf("Hash: ");
-    for (int i = 0; i < 32; i++) {
-        printf("%02x", digest[i]);
-    }
-    printf("\n");
-
-    return 0;
+void mirror256_final(mirror256_context *ctx, uint8_t *digest) {
+    // Process the remaining buffer data and compute the final hash value using the digest() logic from the Python code.
+    // Then, store the result in the "digest" parameter.
 }
