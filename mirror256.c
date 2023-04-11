e343a36dc9a99a6f12baef75d5fa6d45cbc44f00
José <30682875+jo-tm@users.noreply.github.com>
2023-04-11 22:36:14 +0200

    Create mirror256.c

---
+++ mirror256.c
@@ -0,0 +1,16 @@
+#include "mirror256.h"
+#include <string.h>
+
+// Implement the mirror256 functions here.
+
+void mirror256_init(mirror256_context *ctx) {
+    // TODO: Implement the constructor logic here.
+}
+
+void mirror256_update(mirror256_context *ctx, const uint8_t *data, size_t len) {
+    // TODO: Implement the update logic here.
+}
+
+void mirror256_final(mirror256_context *ctx, uint8_t *digest) {
+    // TODO: Implement the final logic here.
+}
