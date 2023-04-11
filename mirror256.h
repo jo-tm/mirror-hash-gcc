ef9ea9925ee77ca7ee2c18bd551d96635a62a2f9
José <30682875+jo-tm@users.noreply.github.com>
2023-04-11 22:46:27 +0200

    Update mirror256.h

--- mirror256.h
+++ mirror256.h
@@ -1,14 +1,23 @@
 #ifndef MIRROR256_H
 #define MIRROR256_H
 
+#include <stddef.h>
 #include <stdint.h>
-#include <stdlib.h>
 
-typedef struct mirror256_context {
-    uint8_t last_hashes[64][32];
-    uint8_t buffer[32];
-    size_t counter;
-    uint8_t hashed[32];
+#define MIRROR256_DEFAULT_DEPTH 64
+#define MIRROR256_DEFAULT_SIZE 256
+
+// Define firstPrimesCubicRootDecRep here
+static const float firstPrimesCubicRootDecRep[] = {
+    // Your firstPrimesCubicRootDecRep values here
+};
+
+typedef struct {
+    uint8_t buffer[MIRROR256_DEFAULT_SIZE / 8];
+    uint64_t counter;
+    uint8_t depth;
+    uint8_t size;
+    uint8_t last_hashes[MIRROR256_DEFAULT_DEPTH][MIRROR256_DEFAULT_SIZE / 4];
 } mirror256_context;
 
 void mirror256_init(mirror256_context *ctx);
